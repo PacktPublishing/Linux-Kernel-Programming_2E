78d0a624d38593c3403bb59c9575103268c59d5f
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-02-14 10:29:32 +0530

    bash strict mode, better msgs

--- ch2/kbuild.sh
+++ ch2/kbuild.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # kbuild.sh
 # Simple kernel build script; minimally tested, YMMV!
+
+# Turn on Bash 'strict mode'! V useful
+# ref: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+
 name=$(basename $0)
 NUMCORES=$(nproc)
 JOBS=$((${NUMCORES}*2))
@@ -12,11 +17,13 @@ BUILD_INSTALL_MOD=1
 	echo "Usage: ${name} path-to-kernel-src-tree-to-build"
 	exit 1
 }
+set +u
 [ ! -z "${ARCH}" ] && {
 	echo "${name}: you seem to want to build the kernel for arch ${ARCH}"
 	echo "This simple script currently supports only x86_64"
 	exit 1
 }
+set -u
 cd $1 || exit 1
 echo "Version: $(head Makefile)"
 
@@ -33,37 +40,42 @@ if [ ${CONFIGURE} -eq 1 ] ; then
   }
   ls -l .config
 else
-  echo "[Skipping kernel configure, just running 'make oldconfig']"
+  echo "[-] Skipping kernel configure, just running 'make oldconfig'"
   echo "[+] make oldconfig"
   make oldconfig  # Update current config utilising a provided .config as base
 fi
+
 # Ensure config is sane
+echo
 echo "[+] scripts/config --disable SYSTEM_REVOCATION_KEYS"
 scripts/config --disable SYSTEM_REVOCATION_KEYS
 
+echo
 echo "[+] time make -j${JOBS}"
 time make -j${JOBS} && echo || {
-  echo "make <kernel> fail"
+  echo "make <kernel> *failed*"
   exit 1
 }
 [ ! -f arch/x86/boot/bzImage ] && {
-  echo "make <kernel> fail? arch/x86/boot/bzImage not gen."
+  echo "make <kernel> *failed*? arch/x86/boot/bzImage not gen."
   exit 1
 }
 
 if [ ${BUILD_INSTALL_MOD} -eq 1 ] ; then
-   echo "[+] sudo make modules_install "
-   sudo make modules_install || { 
-     echo "Fail modules install"
-     exit 1
-   }
+	echo
+	echo "[+] sudo make modules_install "
+	sudo make modules_install || { 
+		echo "*Failed* modules install"
+		exit 1
+	}
 else
-  echo "[Skipping kernel modules build and install step]"
+	echo "[-] Skipping kernel modules build and install step"
 fi
 
+echo
 echo "[+] sudo make install"
 sudo make install || {
-	echo Fail.
+	echo "*Failed*"
 	exit 1
 }
 echo "
