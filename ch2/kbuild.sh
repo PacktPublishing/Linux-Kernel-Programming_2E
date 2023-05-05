360d60127f1d967a2cb288f21f1ac04e2ca2e6d8
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-05-05 11:11:05 +0530

    Add a die() function; syntax cleanup

--- ch2/kbuild.sh
+++ ch2/kbuild.sh
@@ -13,12 +13,17 @@ JOBS=$((${NUMCORES}*2))
 CONFIGURE=1
 BUILD_INSTALL_MOD=1
 
-[ $# -ne 1 ] && {
-	echo "Usage: ${name} path-to-kernel-src-tree-to-build"
-	exit 1
+die()
+{
+echo >&2 "$@"
+exit 1
 }
+
+
+#--- 'main'
+[[ $# -ne 1 ]] && die "Usage: ${name} path-to-kernel-src-tree-to-build"
 set +u
-[ ! -z "${ARCH}" ] && {
+[[ ! -z "${ARCH}" ]] && {
 	echo "${name}: you seem to want to build the kernel for arch ${ARCH}"
 	echo "This simple script currently supports only x86_64"
 	exit 1
@@ -27,17 +32,14 @@ set -u
 cd $1 || exit 1
 echo "Version: $(head Makefile)"
 
-if [ ${CONFIGURE} -eq 1 ] ; then
+if [[ ${CONFIGURE} -eq 1 ]] ; then
   lsmod > /tmp/lsmod.now
   echo "[+] make LSMOD=/tmp/lsmod.now localmodconfig"
   make LSMOD=/tmp/lsmod.now localmodconfig
   echo "[+] make oldconfig"
   make oldconfig  # Update current config utilising a provided .config as base
   echo "[+] make menuconfig "
-  make menuconfig && echo || {
-    echo "menuconfig failed"
-    exit 1
-  }
+  make menuconfig && echo || die "menuconfig failed"
   ls -l .config
 else
   echo "[-] Skipping kernel configure, just running 'make oldconfig'"
@@ -45,39 +47,29 @@ else
   make oldconfig  # Update current config utilising a provided .config as base
 fi
 
-# Ensure config is sane
+# Ensure config is sane; on Ubuntu, SYSTEM_REVOCATION_KEYS being enabled causes
+# the build to fail..
 echo
 echo "[+] scripts/config --disable SYSTEM_REVOCATION_KEYS"
 scripts/config --disable SYSTEM_REVOCATION_KEYS
 
 echo
 echo "[+] time make -j${JOBS}"
-time make -j${JOBS} && echo || {
-  echo "make <kernel> *failed*"
-  exit 1
-}
-[ ! -f arch/x86/boot/bzImage ] && {
-  echo "make <kernel> *failed*? arch/x86/boot/bzImage not gen."
-  exit 1
-}
+time make -j${JOBS} && echo || die "make <kernel> *failed*"
+[[ ! -f arch/x86/boot/bzImage ]] && die "make <kernel> *failed*? arch/x86/boot/bzImage not gen."
 
-if [ ${BUILD_INSTALL_MOD} -eq 1 ] ; then
+if [[ ${BUILD_INSTALL_MOD} -eq 1 ]] ; then
 	echo
 	echo "[+] sudo make modules_install "
-	sudo make modules_install || { 
-		echo "*Failed* modules install"
-		exit 1
-	}
+	sudo make modules_install || die "*Failed* modules install"
 else
 	echo "[-] Skipping kernel modules build and install step"
 fi
 
 echo
 echo "[+] sudo make install"
-sudo make install || {
-	echo "*Failed*"
-	exit 1
-}
+sudo make install || die "*Failed*"
 echo "
-Done, reboot, select your new kernel from the bootloader menu & boot it"
+Done, reboot, select your new kernel from the bootloader menu & boot it
+(If not already done, you first need to configure GRUB to show the menu at boot)"
 exit 0
