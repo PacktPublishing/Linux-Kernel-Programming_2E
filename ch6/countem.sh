545ea1df39c4e191aa8ad0630ea5bffccc43750e
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-07-05 10:11:45 +0530

    alignment adj

--- ch6/countem.sh
+++ ch6/countem.sh
@@ -26,16 +26,16 @@ else
 fi
 
 total_prcs=$(ps -A|wc -l)
-printf "\nTotal # of processes alive              = %9d\n" ${total_prcs}
+printf "\nTotal # of processes alive               = %9d\n" ${total_prcs}
 
 # ps -LA shows all threads
 total_thrds=$(ps -LA|wc -l)
-printf "Total # of threads alive                = %9d\n" ${total_thrds}
+printf "Total # of threads alive                 = %9d\n" ${total_thrds}
 
 # ps aux shows all kernel threads names (col 11) in square brackets; count 'em
 total_kthrds=$(ps aux|awk '{print $11}'|grep "^\["|wc -l)
 
-printf "Total # of kernel threads alive         = %9d\n" ${total_kthrds}
+printf "Total # of kernel threads alive          = %9d\n" ${total_kthrds}
 printf "Thus, total # of user mode threads alive = %9d\n" $((${total_thrds}-${total_kthrds}))
 
 exit 0
