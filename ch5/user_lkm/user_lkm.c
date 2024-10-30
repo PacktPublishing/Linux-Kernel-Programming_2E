b8f13b30146dc74058293020d1b062d20545b3e0
eibisidi <eibisidi@qq.com>
2024-10-29 22:20:36 -0700

    Makefile add KBUILD_EXTRA_SYMBOLS=

---
+++ ch5/user_lkm/user_lkm.c
@@ -0,0 +1,60 @@
+/*
+ * ch5/modstacking/user_lkm.c
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Linux Kernel Programming" 2E
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *  GitHub repository:
+ *  https://github.com/PacktPublishing/Linux-Kernel-Programming_2E
+ *
+ * From: Ch 5: Writing your First Kernel Module- LKMs Part 2
+ ****************************************************************
+ * Brief Description:
+ * This kernel module - user_lkm - is part of the 'modstacking' POC project:
+ *    user_lkm           [<--- this code]
+ *        |
+ *    core_lkm
+ * The user_lkm kernel module calls an (exported) function that resides
+ * in the core_lkm kernel module.
+ *
+ * For details, please refer the book, Ch 5.
+ */
+#define pr_fmt(fmt) "%s:%s(): " fmt, KBUILD_MODNAME, __func__
+
+#include <linux/init.h>
+#include <linux/module.h>
+
+#if 1
+MODULE_LICENSE("Dual MIT/GPL");
+#else
+MODULE_LICENSE("MIT");
+#endif
+
+extern void llkd_sysinfo2(void);
+extern long get_skey(int);
+extern int exp_int;
+
+/* Call some functions within the 'core' module */
+static int __init user_lkm_init(void)
+{
+	u64 sk;
+
+#define THE_ONE   0xfedface
+	pr_info("inserted\n");
+	sk = get_skey(THE_ONE);
+
+	pr_debug("Called get_skey(), ret = 0x%llx = %llu\n", sk, sk);
+	pr_debug("exp_int = %d\n", exp_int);
+	llkd_sysinfo2();
+
+	return 0;
+}
+
+static void __exit user_lkm_exit(void)
+{
+	pr_info("bids you adieu\n");
+}
+
+module_init(user_lkm_init);
+module_exit(user_lkm_exit);
